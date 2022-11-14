2cb8248601d9a8418e5f35f779626da41489d114
Maria <mashamitrankova@gmail.com>
2022-11-14 22:21:09 +0300

    upgraded converter

--- md_converter.py
+++ md_converter.py
@@ -1,18 +1,42 @@
-# Vivelapaix
+DELIMETER = '#<!---end--->'
 
-with open("solution.py") as f:
-    content = f.read()
-    description, code = content.split('# ---end----')
-    lines = description.split('\n')
+def read_data(file):
+    with open(file) as f:
+        content = f.read()
+    return content
+
+def write_data(content, file):
+    with open(file, 'w') as md:
+        md.write(content)
+
+def prepare_data(content):
+    if len(content) > 0:
+        description, code = content.split(DELIMETER)
+    else:
+        description = ''
+        code = ''
+    return description, code
+
+def add_new_data(old_data, data):
+    new_title, new_description = None, None
+    new_des, new_code = prepare_data(data)
+    lines = new_des.split('\n')
     for line in lines:
         if line.startswith('# title'):
-            title = line.split('# title')
+            new_title = line.split('# title')
         if line.startswith('# description'):
-            description = line.split('# description')
+            new_description = line.split('# description')
 
-with open("out.txt", 'w') as md:
-    link = '-'.join(''.join(title[1].lower().split()))
-    out = '+ [{}](#{})\n\n## {}\n\n'.format(title[1], link, description[1])
-    md.write(out)
-    fcode = '```pyhon\n\n{}```'.format(code)
-    md.write(fcode)
+    link = new_title[1].lower().replace(' ', '-')
+    out = '+ [{}](#{})\n\n## {}\n\n'.format(new_title[1], link, new_description[1])
+    fcode = '```python\n{}\n```'.format(new_code)
+    output = old_data + '\n' + out + '\n' + DELIMETER[1:] + '\n' + fcode
+    return output
+
+
+old_data = read_data('solutions.md')
+new_data = read_data('solution.py')
+out = add_new_data(old_data, new_data)
+#print(out)
+write_data(out, 'solutions.md')
+
